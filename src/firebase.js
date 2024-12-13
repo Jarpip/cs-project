1bf826503d72a89c6326987fa9646368b7976e3b
naluedon <naluedon.a@gmail.com>
2024-12-13 10:58:29 +0700

    remove firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyA9LxJGtybe82ER7gu8c7IK9wgIE7eFOqM",
-  authDomain: "first-auto-service.firebaseapp.com",
-  projectId: "first-auto-service",
-  storageBucket: "first-auto-service.appspot.com",
-  messagingSenderId: "10354105582",
-  appId: "1:10354105582:web:e7b1895e0cb257b52aa7ff",
-  measurementId: "G-W3J4ME2EVG",
+  //secret
 };
 
 const app = initializeApp(firebaseConfig);
